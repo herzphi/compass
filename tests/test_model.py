f54ca089ad1f9778a3ea32f3c07c2c014913d258
herzphi <philipp.herz@cellcentric.net>
2023-05-03 10:42:38 +0200

    Updated testfile

--- tests/test_model.py
+++ tests/test_model.py
@@ -41,7 +41,7 @@ def test_host_star_object():
     host_star.cone_gaia_objects(0.1)
     assert len(host_star.cone_gaia) == 9257
     df_bp = host_star.concat_binning_parameters(host_star.cone_gaia, "ks_m_calc")
-    host_star.pmm_parameters([df_bp], "band", None, False)
+    host_star.calc_background_model_parameters([df_bp], "band", None, False)
     assert len(list(host_star.__dict__)) == 49
 
 
